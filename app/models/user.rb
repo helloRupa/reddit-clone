63ad71d50ecbc26a7c1c4ced26290048296ccf12
Rupa Dhillon <rupa211984@gmail.com>
2019-05-22 16:38:20 -0700

    Validate user data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
-# email, string, unique, present
-# username, string, unique, present
-# password_digest, string, unique, present
-# session_token, string, unique, present
-# activated, boolean, unique, present
-# activation_token, string, unique, present
+# email,            string,     unique, present
+# username,         string,     unique, present
+# password_digest,  string,             present
+# session_token,    string,     unique, present
+# activated,        boolean,            present
+# activation_token, string,             present
 
 class User < ApplicationRecord
   attr_reader :password
 
   validates :email, :username, :password_digest, :session_token, :activation_token, presence: true
-  validates :email, :username, :session_token, :activation_token, unique: true
+  validates :email, :username, :session_token, unique: true
+  validates :password, length: { minimum: 8, allow_nil: true }
+  validates :username, length: { minimum: 3 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def password=(pw)
     @password = pw
