40dcb0d3057b632b37a142946a12009c8cf7ad46
Rupa Dhillon <rupa211984@gmail.com>
2019-05-22 17:10:15 -0700

    Route Users and Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show, :destroy] do
+    get 'activate', on: :collection
+  end
+  
+  resource :session, only: [:new, :create, :destroy]
 end
