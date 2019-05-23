ae4c4873ce7b17f999d2a3740a8b89ab6ed33045
Rupa Dhillon <rupa211984@gmail.com>
2019-05-22 17:01:58 -0700

    Add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,42 @@ class User < ApplicationRecord
   attr_reader :password
 
   validates :email, :username, :password_digest, :session_token, :activation_token, presence: true
-  validates :email, :username, :session_token, unique: true
+  validates :email, :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 8, allow_nil: true }
   validates :username, length: { minimum: 3 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
+  after_initialize :ensure_session_token, :ensure_activation_token
+
   def password=(pw)
     @password = pw
     self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def ensure_activation_token
+    self.activation_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(email, pw)
+    user = User.find_by_email(email)
+    user && user.is_password?(pw) ? user : nil
   end
 end
