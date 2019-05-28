cf89f26c9ab96309bd8923d1666a38754ff31696
Rupa Dhillon <rupa211984@gmail.com>
2019-05-27 17:07:18 -0700

    Nest comments

--- app/assets/javascripts/components/nested_comments.js
+++ app/assets/javascripts/components/nested_comments.js
@@ -3,6 +3,7 @@ const replyBtns = Array.from(document.getElementsByClassName('reply-btn'));
 replyBtns.forEach((btn) => {
     btn.onclick = (e) => {
         const siblingId = e.target.id.replace('comment', 'form');
-        document.getElementById(siblingId).classList.remove('hide');
+        document.getElementById(siblingId).classList.toggle('hide');
+        e.preventDefault();
     };
 });
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,25 @@
 module CommentsHelper
+  def comments_hash(comments)
+    hash = Hash.new { |h, k| h[k] = [] }
+
+    comments.each { |c| hash[c.parent_comment_id] << c }
+    hash
+  end
+
+  def nested_comments(comments)
+    return nil if comments.empty?
+    hash = comments_hash(comments)
+    inner_comments(hash, nil).html_safe
+  end
+
+  def inner_comments(hash, comment_id)
+    str = ''
+    return str unless hash.key?(comment_id)
+
+    hash[comment_id].each do |comment|
+      str += '<div class="indent">' + render("comments/single_comment", comment: comment)
+      str += inner_comments(hash, comment.id) + '</div>'
+    end
+    str
+  end
 end
