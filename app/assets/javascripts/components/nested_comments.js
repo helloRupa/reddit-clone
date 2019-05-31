f78b53136f8acb4c58ebf1f1b2caa450fa4de0d8
Rupa Dhillon <rupa211984@gmail.com>
2019-05-30 18:36:14 -0700

    Show and scroll to comment upon save

--- app/assets/javascripts/components/nested_comments.js
+++ app/assets/javascripts/components/nested_comments.js
@@ -1,19 +1,39 @@
 const replyBtns = Array.from(document.getElementsByClassName('reply-btn'));
 const showRepliesBtns = Array.from(document.getElementsByClassName('show-replies'));
+const url = new URL(window.location.href);
+const cId = url.searchParams.get('show_cid');
 
 replyBtns.forEach((btn) => {
     btn.onclick = (e) => {
         const siblingId = e.target.id.replace('comment', 'form');
         document.getElementById(siblingId).classList.toggle('hide');
-        // e.preventDefault();
     };
 });
 
 showRepliesBtns.forEach((btn) => {
     btn.onclick = (e) => {
-        const comments = Array.from(e.target.parentElement.getElementsByClassName('indent'));
+        const comments = Array.from(e.target.parentElement.getElementsByClassName('flat'));
         comments.forEach((c, idx) => {
             if(idx > 0) c.classList.toggle('hide');
         });
     };
 });
+
+if(cId) {
+    const cDiv = document.getElementById(cId);
+
+    if (cDiv.parentElement.classList.contains('flat')) {
+        let parent = cDiv.parentElement;
+
+        while(!parent.classList.contains('parent')){
+            parent = parent.parentElement;
+        }
+
+        Array.from(parent.getElementsByClassName('flat')).forEach((c, idx) => {
+            if(idx > 0) c.classList.toggle('hide');
+        });
+    }
+
+    cDiv.scrollIntoView();
+}
+
