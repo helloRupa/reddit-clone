b284159c2bc64df0792080dc84f69c5e28236cc3
Rupa Dhillon <rupa211984@gmail.com>
2019-05-30 20:51:15 -0700

    Show latest 20 posts on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @posts = Post.all.create_order(:desc).includes(:votes, :author).limit(20)
     render :index
   end
 end
---
+++ db/migrate/20190531035044_create_subscriptions.rb
@@ -0,0 +1,6 @@
+class CreateSubscriptions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+    end
+  end
+end
