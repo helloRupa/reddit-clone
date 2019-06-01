bc5870459d9c5af4a1adb5b90cd1065c9c012e71
Rupa Dhillon <rupa211984@gmail.com>
2019-05-31 17:16:07 -0700

    Check checkboxes via datalist input

---
+++ app/assets/javascripts/components/post_form.js
@@ -0,0 +1,14 @@
+const input = document.getElementById('sub-choices');
+const options = input.list.options;
+const optionsArr = Array.from(options).map((el) => el.value);
+
+input.oninput = (e) => {
+    let value = e.target.value;
+    let idx = optionsArr.indexOf(value);
+    
+    if(idx > -1) {
+        const checkbox = document.getElementById(options[idx].dataset.subid);
+        checkbox.checked = true;
+//         checkbox.parentElement.classList.remove('hide');
+    }
+};
