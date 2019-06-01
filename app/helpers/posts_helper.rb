c4380a920d0eaa731dee09a4651a86e7bb90c96c
Rupa Dhillon <rupa211984@gmail.com>
2019-05-31 18:18:43 -0700

    Clean up upvote_percentage

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,10 +4,7 @@ module PostsHelper
     return 0 if total_votes.zero?
 
     score = post.vote_total * 1.0
-    if score > 0
-      ( ( (total_votes - score) / 2 ) + score ) / total_votes * 100
-    else
-      ( (total_votes + score) / 2 ) / total_votes * 100
-    end
+    positive = score > 0 ? ( ( (total_votes - score) / 2 ) + score ) : ( (total_votes + score) / 2 )
+    positive / total_votes * 100
   end
 end
