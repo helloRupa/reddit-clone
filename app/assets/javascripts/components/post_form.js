f9df8a6b41dcf3bb902586bae22159a222aa7ca8
Rupa Dhillon <rupa211984@gmail.com>
2019-06-03 01:25:52 -0700

    Style Post edit and new form

--- app/assets/javascripts/components/post_form.js
+++ app/assets/javascripts/components/post_form.js
@@ -4,17 +4,6 @@ const optionsArr = Array.from(options).map((el) => el.value.toLowerCase());
 const addSubBtn = document.getElementById('add-sub');
 const errorDiv = document.getElementById('no-match');
 
-// input.oninput = (e) => {
-//     let value = e.target.value;
-//     let idx = optionsArr.indexOf(value);
-    
-//     if(idx > -1) {
-//         const checkbox = document.getElementById(options[idx].dataset.subid);
-//         checkbox.checked = true;
-//         checkbox.parentElement.classList.remove('hide');
-//     }
-// };
-
 addSubBtn.onclick = (e) => {
     let value = input.value.toLowerCase();
     let idx = optionsArr.indexOf(value);
@@ -26,7 +15,7 @@ addSubBtn.onclick = (e) => {
         input.value = '';
     }
     else {
-        input.setCustomValidity('Sub does not exist');
+        // input.setCustomValidity('Sub does not exist');
         errorDiv.classList.remove('invisible');
     }
 
@@ -34,6 +23,6 @@ addSubBtn.onclick = (e) => {
 };
 
 input.oninput = () => {
-    input.setCustomValidity('');
+    // input.setCustomValidity('');
     if(!errorDiv.classList.contains('invisible')) errorDiv.classList.add('invisible');
 };
