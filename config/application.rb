e5ccf6f7a2a48bd662dc703ef69af082862068e8
Rupa Dhillon <rupa211984@gmail.com>
2019-06-05 16:18:09 -0700

    Serve assets

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module RedditClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
