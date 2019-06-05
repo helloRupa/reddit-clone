028a0938c212296eca771c389a74bd962c986be2
Rupa Dhillon <rupa211984@gmail.com>
2019-06-04 19:28:02 -0700

    Style user show page and comment show

--- app/assets/javascripts/components/modal.js
+++ app/assets/javascripts/components/modal.js
@@ -1,9 +1,15 @@
+const modal = document.getElementById('delete-account');
+const overlay = document.getElementById('overlay');
+const body = document.body;
+
 document.getElementById('delete_user').onclick = () => {
-    const modal = document.getElementById('delete-account');
+    overlay.classList.remove('hide');
     modal.classList.remove('hide');
+    body.classList.add('no-scroll');
 };
 
 document.getElementById('keep_account').onclick = () => {
-    const modal = document.getElementById('delete-account');
+    overlay.classList.add('hide');
     modal.classList.add('hide');
+    body.classList.remove('no-scroll');
 };
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -6,44 +6,77 @@ module CommentsHelper
     hash
   end
 
+  # def formatted_comments(comments, start_key, type = 'classic', hide = true)
+  #   return nil if comments.empty?
+  #   hash = comments_hash(comments)
+
+  #   return threaded_comments(hash, start_key).html_safe if type == 'classic'
+  #   flat_comments(hash, start_key, 'blue', hash[start_key].first, hide).html_safe
+  # end
+
   def formatted_comments(comments, start_key, type = 'classic', hide = true)
     return nil if comments.empty?
     hash = comments_hash(comments)
 
     return threaded_comments(hash, start_key).html_safe if type == 'classic'
-    flat_comments(hash, start_key, 'blue', hash[start_key].first, hide).html_safe
+    flat_comments(hash, start_key, hash[start_key].first, hide).html_safe
   end
 
-  def flat_comments(hash, comment_id, color = 'blue', parent = nil, hide = true)
+  def flat_comments(hash, comment_id, parent = nil, hide = true, level = 'talk1')
     str = ''
     return str unless hash.key?(comment_id)
 
-    color = ['blue', 'red'].include?(color) ? 'grey' : 'blue'
+    level = ['talk1', 'talk_break'].include?(level) ? 'talk2' : 'talk1'
 
     hash[comment_id].each_with_index do |comment, idx|
       display = 'hide'
       if comment_id.nil?
         display = nil
-        color = 'blue'
+        level = 'talk1'
         str += "#{'</div>' unless idx.zero?}<div class=\"parent\">#{replies_btn if hash.key?(comment.id)}"
       end
-      # str += "<div class=\"indent flat #{color} container #{display if hide}\">#{parent_link(parent, color)}#{render("comments/single_comment", comment: comment)}</div>"
-      str += "<div class=\"indent flat #{color} container #{display if hide}\">#{render("comments/single_comment", comment: comment, quote: parent_link(parent, color))}</div>"
-      str += flat_comments(hash, comment.id, color, comment, hide)
-      color = 'red'
+      str += "<div class=\"indent flat #{level} container #{display if hide}\">#{render("comments/single_comment", comment: comment, quote: parent_link(parent, level))}</div>"
+      str += flat_comments(hash, comment.id, comment, hide, level)
+      level = 'talk_break'
     end
     str
   end
+
+  # def flat_comments(hash, comment_id, color = 'blue', parent = nil, hide = true)
+  #   str = ''
+  #   return str unless hash.key?(comment_id)
+
+  #   color = ['blue', 'red'].include?(color) ? 'grey' : 'blue'
+
+  #   hash[comment_id].each_with_index do |comment, idx|
+  #     display = 'hide'
+  #     if comment_id.nil?
+  #       display = nil
+  #       color = 'blue'
+  #       str += "#{'</div>' unless idx.zero?}<div class=\"parent\">#{replies_btn if hash.key?(comment.id)}"
+  #     end
+  #     str += "<div class=\"indent flat #{color} container #{display if hide}\">#{render("comments/single_comment", comment: comment, quote: parent_link(parent, color))}</div>"
+  #     str += flat_comments(hash, comment.id, color, comment, hide)
+  #     color = 'red'
+  #   end
+  #   str
+  # end
 
   def replies_btn
     '<button class="show-replies">Replies</button>'
   end
 
-  def parent_link(parent, color)
-    return nil if parent.nil? || color != 'red'
+  def parent_link(parent, level)
+    return nil if parent.nil? || level != 'talk_break'
     link = link_to('More', comment_url(parent), target: "_blank")
-    "<div class=\"quote\">\"#{parent.quote}\" - #{link}</div>"
+    "\"#{parent.quote}\" - #{link}"
   end
+
+  # def parent_link(parent, color)
+  #   return nil if parent.nil? || color != 'red'
+  #   link = link_to('More', comment_url(parent), target: "_blank")
+  #   "<div class=\"quote\">\"#{parent.quote}\" - #{link}</div>"
+  # end
 
   def threaded_comments(hash, comment_id)
     str = ''
